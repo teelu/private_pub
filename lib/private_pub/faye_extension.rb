9df1cd2c08bac2c78e8d79ed5cf796bc0fcaf60e
teelu <terrence.lun@gmail.com>
2015-09-13 10:22:28 +0800

    Removed Validation

--- lib/private_pub/faye_extension.rb
+++ lib/private_pub/faye_extension.rb
@@ -18,21 +18,12 @@ module PrivatePub
     # Ensure the subscription signature is correct and that it has not expired.
     def authenticate_subscribe(message)
       subscription = PrivatePub.subscription(:channel => message["subscription"], :timestamp => message["ext"]["private_pub_timestamp"])
-      if message["ext"]["private_pub_signature"] != subscription[:signature]
-        message["error"] = "Incorrect signature."
-      elsif PrivatePub.signature_expired? message["ext"]["private_pub_timestamp"].to_i
-        message["error"] = "Signature has expired."
-      end
     end
 
     # Ensures the secret token is correct before publishing.
     def authenticate_publish(message)
       if PrivatePub.config[:secret_token].nil?
         raise Error, "No secret_token config set, ensure private_pub.yml is loaded properly."
-      elsif message["ext"]["private_pub_token"] != PrivatePub.config[:secret_token]
-        message["error"] = "Incorrect token."
-      else
-        message["ext"]["private_pub_token"] = nil
       end
     end
   end
